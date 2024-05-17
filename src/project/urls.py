3ad082290e90d7fa13e7051eea9583e25edd0726
Osayi Akoko <osayistreams@gmail.com>
2024-05-17 23:19:22 +0100

    Impl doc app

--- src/project/urls.py
+++ src/project/urls.py
@@ -19,8 +19,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("v1/account/", include("account.urls")),
     path("v1/auth/", include("authentication.urls")),
     path("v1/tasks/", include("task.urls")),
+    path("admin/", admin.site.urls),
+    path("", include("doc.urls")),
 ]
