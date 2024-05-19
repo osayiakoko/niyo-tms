00c1677874b482a96a9a01bb3010631c8a6b7997
Osayi Akoko <osayistreams@gmail.com>
2024-05-19 18:12:07 +0100

    setup channels

--- src/project/settings/components/common.py
+++ src/project/settings/components/common.py
@@ -32,6 +32,7 @@ CSRF_TRUSTED_ORIGINS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
