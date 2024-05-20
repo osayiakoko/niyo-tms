b27c2ad24f12fde56387efa63680a77ea8faaa29
Osayi Akoko <osayistreams@gmail.com>
2024-05-20 04:08:34 +0100

    add prod & staging env default config

---
+++ src/project/settings/environments/staging.py
@@ -0,0 +1,12 @@
+# pylint: skip-file
+from core.enums import ProjectEnv
+
+
+PROJECT_ENV = ProjectEnv.STAGING
+
+ALLOWED_HOSTS = []
+
+CSRF_TRUSTED_ORIGINS = []
+
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = []
